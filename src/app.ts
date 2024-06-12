7726c74c31a04b3a6c272ae7b08488d993351ea8
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-06-12 10:56:48 -0300

    chore: enabling the app to access users routes and setting a prefix

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+import { userRoutes } from './routes/auth.routes';
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 
 export const app = fastify()
 
 app.register(cookie)
+app.register(userRoutes, {prefix: "user"})
